14c9dc6a61b87c4685f8c7f2fb8039674eb98555
kubya <153851249+kubya@users.noreply.github.com>
2023-12-18 04:45:32 +0100

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,108 +6,78 @@
 /*   By: ndesques <nefdesq@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 00:03:40 by ndesques          #+#    #+#             */
-/*   Updated: 2023/11/09 00:03:40 by ndesques         ###   ########.fr       */
+/*   Updated: 2023/12/18 04:42:13 by ndesques         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_strnum(char *str, char c)
+static size_t	ft_strnum(char const *str, char c)
 {
-	int	i;
-	int	res;
+	size_t	i;
+	size_t	res;
 
 	i = 0;
 	res = 0;
 	while (str[i])
 	{
-		if (str[i] != c)
-		{
+		if (str[i] != c && (str[i + 1] == c || str[i + 1] == '\0'))
 			res++;
-			while (str[i] && str[i] != c)
-				i++;
-			if (str[i] == '\0')
-				return (res);
-		}
 		i++;
 	}
 	return (res);
 }
 
-static void	ft_strcpy(char *dest, char *str, char c, int j)
+static void	ft_strcpy(char *dest, char const *str, char c)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
-	while (str[j] && str[j] == c)
-		j++;
-	while (str[j + i] && str[j + i] != c)
+	while (str[i] && str[i] != c)
 	{
-		dest[i] = str[j + i];
+		dest[i] = str[i];
 		i++;
 	}
 	dest[i] = '\0';
 }
 
-static char	*ft_walloc(char *str, char c, int *j)
+static void	ft_walloc(char const *str, char **tab, char c)
 {
-	char	*dest;
-	int		i;
-
-	i = *j;
-	dest = NULL;
-	while (str[*j])
-	{
-		if (str[*j] != c)
-		{
-			while (str[*j] && str[*j] != c)
-				*j += 1;
-			dest = (char *)malloc((*j - i + 1) * sizeof(char));
-			if (dest == NULL)
-				return (NULL);
-			break ;
-		}
-		*j += 1;
-	}
-	ft_strcpy(dest, str, c, i);
-	return (dest);
-}
-
-static void	ft_free(char *strings)
-{
-	int	i;
+	size_t	i;
+	size_t	y;
+	size_t	num;
 
 	i = 0;
-	while (strings[i])
+	y = 0;
+	while (str[y])
 	{
-		free(strings);
-		i++;
+		num = 0;
+		while (str[y + num] && str[y + num] != c)
+			num++;
+		if (num > 0)
+		{
+			tab[i] = malloc(sizeof(char) * (num + 1));
+			if (!tab[i])
+				return ;
+			ft_strcpy(tab[i], (str + y), c);
+			i++;
+			y = y + num;
+		}
+		else
+			y++;
 	}
-	free(strings);
+	tab[i] = 0;
 }
 
 char	**ft_split(char const *str, char c)
 {
 	char	**tab;
-	int		i;
-	int		j;
-	int		k;
+	size_t	num;
 
-	if (str == NULL)
+	num = ft_strnum(str, c);
+	tab = malloc((num + 1) * sizeof(char *));
+	if (!tab)
 		return (NULL);
-	i = 0;
-	j = ft_strnum((char *)str, c);
-	k = 0;
-	tab = (char **)malloc((j + 1) * sizeof(char *));
-	if (tab == NULL)
-		return (NULL);
-	tab[j] = NULL;
-	while (i < j)
-	{
-		tab[i] = ft_walloc(((char *)str), c, &k);
-		if (!tab[i])
-			ft_free(tab[i]);
-		i++;
-	}
+	ft_walloc(str, tab, c);
 	return (tab);
 }
